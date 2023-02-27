3ea373b3bd15363159ca4bb208af94d5e9c29c8d
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-02-26 22:19:35 -0300

    Impedindo que cadastre uma conta igual
    Coloquei pra verificar se tinha uma conta com senha, ou n_usuario, ou email igual no banco, pois ai não realizaria o cadastro.

--- controller/salvarUsu.php
+++ controller/salvarUsu.php
@@ -9,15 +9,30 @@ $n_usuario = $_POST['n_usuario'];
 $email_usuario = $_POST['email_usuario'];
 $senha_usuario = $_POST['senha_usuario'];
 
-// Inserindo informações no banco
+//Verificando se a conta já existe
 
-$stmt = $dbh->prepare('INSERT INTO T_usuario (idT_usuario,nomeCompleto_usuario,n_usuario,email,senha)' . 'VALUES(null,:nome_completo,:n_usuario,:email_usuario,:senha_usuario)');
-$stmt->execute(Array(
-    ':nome_completo' => $nome_completo,
+$stmt2 = $dbh ->prepare('SELECT * FROM T_usuario WHERE n_usuario = :n_usuario OR senha = :senha_usuario OR email = :email_usuario');
+$stmt2->execute(Array(
     ':n_usuario' => $n_usuario,
     ':email_usuario' => $email_usuario,
-    ':senha_usuario' => $senha_usuario,
-));
+    ':senha_usuario' => $senha_usuario,));
+
+$res = $stmt2->rowCount();
+    if($res==null){
+
+// Inserindo informações no banco
+
+        $stmt = $dbh->prepare('INSERT INTO T_usuario (idT_usuario,nomeCompleto_usuario,n_usuario,email,senha)' . 'VALUES(null,:nome_completo,:n_usuario,:email_usuario,:senha_usuario)');
+        $stmt->execute(Array(
+        ':nome_completo' => $nome_completo,
+        ':n_usuario' => $n_usuario,
+        ':email_usuario' => $email_usuario,
+        ':senha_usuario' => $senha_usuario,
+        ));
+    }else{
+        header("Location: http://localhost/GraviBaby/view/teste.php");
+    }
+
 
 //Voltando a página de login
 
