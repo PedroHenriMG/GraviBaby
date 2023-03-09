d91a0d043285891e54ff1b5f1765162a35194957
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-03-09 17:46:43 -0300

    Agora nomes de usuários são únicos
    Posteriormente será mudado para o email ser único.

--- controller/editar_perfil.php
+++ controller/editar_perfil.php
@@ -11,10 +11,51 @@ $bio1 = $_POST['bio1'];
 $bio2 = $_POST['bio2'];
 $bio3 = $_POST['bio3'];
 
-$sql = "UPDATE T_usuario SET nomeCompleto_usuario = '$nome_completo', n_usuario = '$n_usuario', email = '$email', senha = '$senha', bio1 = '$bio1', bio2 = '$bio2', bio3 = '$bio3' WHERE idT_usuario = $id_usuario";
+$n_usuarioIguais = 0;
 
-$prepare = $dbh->prepare($sql);
-$res = $prepare->execute();
+$sql2 = "SELECT * FROM T_usuario";
+$prepare2 = $dbh->prepare($sql2);
+$exec2 = $prepare2->execute();
 
-header("location: http://localhost/GraviBaby/view/perfil.php");
+$res2 = $prepare2->fetchAll();
+
+foreach($res2 as $linha2){
+    if($linha2['n_usuario']== $n_usuario){
+        $n_usuarioIguais++;
+    }
+}
+
+    if($n_usuarioIguais > 1){
+
+        $sql3 = "SELECT n_usuario FROM T_usuario WHERE idT_usuario = $id_usuario";
+        $prepare3 = $dbh->prepare($sql3);
+        $exec3 = $prepare3->execute();
+        $res3 = $prepare3->fetch(PDO::FETCH_ASSOC);
+        
+            if($res3['n_usuario'] == $n_usuario){
+                echo $res3['n_usuario'] . $n_usuario;
+
+                $sql = "UPDATE T_usuario SET nomeCompleto_usuario = '$nome_completo', n_usuario = '$n_usuario', email = '$email', senha = '$senha', bio1 = '$bio1', bio2 = '$bio2', bio3 = '$bio3' WHERE idT_usuario = $id_usuario";
+
+                $prepare = $dbh->prepare($sql);
+                $res = $prepare->execute();
+                header("Location: http://localhost/GraviBaby/view/editar.php");
+
+            }else{
+                echo "nome de usuario já existe";
+                
+            }
+        
+            
+    }else{
+        $sql = "UPDATE T_usuario SET nomeCompleto_usuario = '$nome_completo', n_usuario = '$n_usuario', email = '$email', senha = '$senha', bio1 = '$bio1', bio2 = '$bio2', bio3 = '$bio3' WHERE idT_usuario = $id_usuario";
+
+        $prepare = $dbh->prepare($sql);
+        $res = $prepare->execute();
+
+        header("Location: http://localhost/GraviBaby/view/editar.php");
+    }
+
+
+
 
