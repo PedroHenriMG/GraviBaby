69fe5f690a38937bfbcf2e1f43c3f557b9ed6150
Pedro Henrique MG <pedromrm1234@gmail.com>
2023-05-29 17:43:54 -0300

    Concertando o erro ao fazer o post, aumentando o tamanho de imagens ao fazer o post e adicionando os formatos de imagem gif e jpeg

--- controller/adicionar_post.php
+++ controller/adicionar_post.php
@@ -19,8 +19,8 @@ if (isset($_FILES['picture__input'])) {
     $pasta = "../postagens/";
 
     $arquivo = $_FILES['picture__input'];
-    if ($arquivo['size'] > 2097000) {
-        die("arquivo muito grande max 2mb");
+    if ($arquivo['size'] > 10097000) {
+        die("arquivo muito grande max 10mb");
     }
     $nomeArquivo = $arquivo['name'];
 
@@ -28,7 +28,7 @@ if (isset($_FILES['picture__input'])) {
 
     $extensao = strtolower(pathinfo($nomeArquivo, PATHINFO_EXTENSION));
 
-    if ($extensao != "jpg" && $extensao != "png" && $extensao != "") {
+    if ($extensao != "jpg" && $extensao != "png" && $extensao != "" && $extensao != "gif" && $extensao != "jpeg") {
         die();
     }
 
@@ -37,15 +37,15 @@ if (isset($_FILES['picture__input'])) {
     $deu_certo = move_uploaded_file($arquivo["tmp_name"], $path);
 
     if ($deu_certo) {
-        $sql17 = "INSERT INTO T_publicacoes (foto, idT_publicacao, descricao, titulo, statuspost, id_usuario, id_grupo) VALUES (:path, null, :descricao_postagem, :titulo_postagem, :statuspost, :id_usuario, 1)";
+        $sql17 = "INSERT INTO T_publicacoes (foto, idT_publicacao, descricao, titulo, status, id_usuario, id_grupo) VALUES (:caminho, null, :descricao_postagem, :titulo_postagem, :statuspost, :id_usuario, 1)";
         $prepare17 = $dbh->prepare($sql17);
-        $prepare17->bindParam(':path', $path);
+        $prepare17->bindParam(':caminho', $path);
         $prepare17->bindParam(':descricao_postagem', $descricao_postagem);
         $prepare17->bindParam(':titulo_postagem', $titulo_postagem);
-        $prepare17->bindParam(':statuspostw', $status);
+        $prepare17->bindParam(':statuspost', $statuspost);
         $prepare17->bindParam(':id_usuario', $id_usuario);
         $exec17 = $prepare17->execute();
     }
 }
 
-header("Location: ../view/postmaker.php");
+header("Location: ../view/home/home.php");
--- view/postmaker.php
+++ view/postmaker.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['id']) && !isset($_SESSION['nome'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>fazer um post</title>
-    <link rel="stylesheet" href="./home/bootstrap-5.2.3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../bootstrap-5.2.3-dist/css/bootstrap.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="../css_normal/postmaker.css">
