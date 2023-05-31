dccdafb588803b4d0907478e60c86a4628a95519
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-05-31 17:40:26 -0300

    arrumando feed

--- componentes/feed.php
+++ componentes/feed.php
@@ -13,7 +13,7 @@ foreach ($resPostagens as $linha_post) {
     $id_publicacao = $linha_post['idT_publicacao'];
 
     $sqlLikes = "SELECT * FROM T_like WHERE idT_publicacao = $id_publicacao";
-    $sqlusuario = "SELECT * FROM T_usuario WHERE idT_usuario = :Id";
+   
 
     $prepareLikes = $dbh->prepare($sqlLikes);
     $execLikes = $prepareLikes->execute();
@@ -25,7 +25,14 @@ foreach ($resPostagens as $linha_post) {
     <div style="margin-top: var(--altura-tamanho); margin-bottom: var(--altura-tamanho);" class="row d-flex justify-content-center text-center align-items-center">
         <div id="Card" class="row d-flex justify-content-center text-center col-xl-6" style='background-image: url("../<?php echo $linha_post['foto'] ?>"); width: 100%; height: 500px;'>
             <div style="height: 30px;" class="mt-3 col-10 text-start">
-                <p> <?php echo $linha_post['id_usuario'] ?></p>
+            <?php
+            $idUsuPost = $linha_post['id_usuario'];
+            $queryUsuPost = $dbh->query("SELECT * FROM T_usuario WHERE idT_usuario = $idUsuPost");
+            $resUsuPost = $queryUsuPost->fetchAll();
+            foreach($resUsuPost as $linha_usu_post){
+            
+            ?>
+                <p> <?php echo $linha_usu_post['n_usuario'] ?></p>
             </div>
             <svg style="height: 30px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mt-3 col-2 bi bi-three-dots-vertical" viewBox="0 0 16 16">
                 <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
@@ -84,10 +91,10 @@ foreach ($resPostagens as $linha_post) {
 
         </div>
         <div class="row mt-3">
-            <p class="col-12 text-start"> <strong>Usuario <?php echo $linha_post['id_usuario'] ?></strong> <?php echo $linha_post['descricao'] ?> </p>
+            <p class="col-12 text-start"> <strong><?php echo $linha_usu_post['n_usuario'] ?></strong> <?php echo $linha_post['descricao'] ?> </p>
         </div>
     </div>
-<?php } ?>
+<?php }} ?>
 
 
 <div id="res" class="caixaComentarios" style="background-color: black; display: none; width:20vw; height: 40vw; position: fixed; right:40%; top:30%">
