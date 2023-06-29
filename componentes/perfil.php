b992f3796987a140d0bd49f63c0935f1dd150646
Carlos Eduardo <ceinvestimentosllc@gmail.com>
2023-06-29 16:48:18 -0300

    Bug do commit corrigido

--- componentes/perfil.php
+++ componentes/perfil.php
@@ -45,6 +45,7 @@ include_once '../controller/util_php/infos_usuario.php';
         <span class="material-symbols-outlined d-flex justify-content-center align-items-center">settings</span>
     </div>
     <div id="caixa-box-3" class="col-12 d-flex justify-content-center">
+    <?php echo "<h1>" . $foto . "</h1>" ?>
         <img id="hexagon" src="<?php
             if($foto != ""){
                 echo $foto;
@@ -54,21 +55,17 @@ include_once '../controller/util_php/infos_usuario.php';
             
         
         ?>" alt="">
-        <?php 
-            if($controle == false){
-         ?>
 
-         <?php if($idUsu != $id_padrao){
-            $display_edit = "display: none;";
-         }else{
-            $display_edit = "display: flex;";
-            $controle = true;
-         }
-            ?>
-            <div id="Edite" style="<?php echo $display_edit ?>" onclick="x()"><img src="../imagens/brush-fill.svg" alt=""></div>
-         
+         <?php 
+            if($idUsu != $id_padrao){
+                $display_edit = "display: none;";
+            }else{
+                $display_edit = "display: flex;";
+            }
+        ?>
 
-        <?php } ?>
+        <div id="Edite" style="<?php echo $display_edit ?>" onclick="x()"><img src="../imagens/brush-fill.svg" alt=""></div>
+
     </div>
     <div id="caixa-box-4" class="col-12 d-flex flex-column justify-content-center align-items-center">
         <h1><?php echo $nomeCompleto_usuario; ?></h1>
@@ -77,17 +74,18 @@ include_once '../controller/util_php/infos_usuario.php';
             <p class="text-center"><strong><?php echo $seguidores; ?> <br> Seguidores</strong></p>
             <p class="text-center"><strong><?php echo $seguindo; ?> <br> Seguindo</strong></p>
         </div>
+
         <?php 
-            if($controle == false){
+            if($controle == true){
          ?>
-         
+
         <form method="post" action="../controller/seguir.php" class="col-6 d-flex justify-content-around">
 
             <input style="display: none;" name="id_usuario" type="text" value="<?php echo $id_padrao ?>">
             <input style="display: none;" name="id_amigo" type="text" value="<?php echo $idUsu ?>">
             
             <?php if($segue == true){?>
-            <button class="Botao btn btn-secondary" style="background-color: cise;" disabled ><strong>Seguindo</strong></button>
+            <button class="Botao btn btn-secondary" style="background-color: cise;" type="submit"><strong>Seguindo</strong></button>
             <?php }else if($segue == false){?>
             <button class="Botao" name="" style="background-color: blueviolet;" type="submit"><strong>Seguir</strong></button>
             <?php }?>
